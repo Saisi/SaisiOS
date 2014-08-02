bed545a389f4e62190bea049fe40ea4311d7cf9d
Saisi <saisi@users.noreply.github.com>
2014-08-02 23:20:43 +0300

    Cleaning up

--- breakout_hex.asm
+++ breakout_hex.asm
@@ -7,11 +7,13 @@ mov bx, 0x1fb6
 mov cx, bx
 and cx, 0x0f
 mov al, cl
+add al,0x30
 
 int 0x10
 
 sub bl, al
 shr bl, 4
+add al,0x30
 mov al, bl
 
 int 0x10
@@ -21,12 +23,14 @@ mov cx, bx
 shr cx, 8
 mov al, cl
 and al, 0x0f
+add al,0x30
 
 int 0x10
 
 sub cl, al
 mov al, cl
 shr al, 4
+add al,0x30
 
 int 0x10
 
