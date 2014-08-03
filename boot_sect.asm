64e58d8dee6bb598d48254f34c5a477e4c2fd27e
Saisi <saisi@users.noreply.github.com>
2014-08-04 02:07:23 +0300

    I know the hustle so well

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,15 +1,32 @@
 [org 0x7c00]
 
-mov bx, HELLO_MSG
-call print_string
+mov [BOOT_DRIVE], dl
+
+;stack
+mov bp, 0x9800
+mov sp, bp
+
+mov bx, 0x9900
+mov es, bx
+mov bx, 0xa000 
+
+mov dh, 2
+mov dl, [BOOT_DRIVE]
+
+call disk_load
 
 jmp $
 
 
-%include 'print.asm'
+%include "print.asm"
+%include "print_hex.asm"
+%include "disk_load.asm"
 
-TO_PRINT:
-	db 'jallo',0
+
+BOOT_DRIVE db 0x00
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+times 256 dw 0xdada
+times 256 dw 0xface
---
+++ disk_load.asm
@@ -0,0 +1,37 @@
+disk_load:
+
+	push dx
+
+	mov ah, 0x02;read sector function
+
+	mov al, 0x02 ;number of sectors
+	mov ch, 0x00 ;cylinder 0
+	mov dh, 0x00 ;track 0
+	mov cl, 0x02 ;sector 2 (base of 2)
+
+	int 0x13
+
+	jc disk_error
+	pop dx
+	cmp dh, al
+	jne disk_error2
+
+
+
+
+	ret
+
+
+disk_error:
+	mov bx, TO_PRINT
+	call print_string
+	jmp $
+
+disk_error2:
+	mov bx, TO_PRINT2
+	call print_string
+	jmp $
+
+
+TO_PRINT db "Disk Read Error",0
+TO_PRINT2 db "Disk Read Error2",0
