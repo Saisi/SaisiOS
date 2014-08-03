9e520147b9a25c4473af90287953d2d1dbf2fe96
Saisi <saisi@users.noreply.github.com>
2014-08-04 01:35:51 +0300

    fixed both prints I think

--- driver_print_hex.asm
+++ driver_print_hex.asm
@@ -1,20 +1,26 @@
 [org 0x7c00]
 
 
-mov dx, [TO_PRINT]
+;stack stuff
+mov bp, 0x9900
+mov sp, bp
+
+;the hex to print
+mov dx, 0x0f01
 call print_hex
 
-	
 
 jmp $
 
-
 %include 'print_hex.asm'
+%include 'print.asm'
 
-TO_PRINT:
-	dd 0x1fb6
 
 
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+
+
+
--- print_hex.asm
+++ print_hex.asm
@@ -1,42 +1,48 @@
 print_hex:
 	pusha
-
-
-	table db '0123456789ABCDEF'  
+	
 	mov bx, table
-
 	mov ah, 0x0e
-	mov cl, dh
 
-	;expects dx to be loaded
+
 	get_first:
-		mov al, cl
+		mov al, dh
 		shr al, 4
 		xlat
 		int 0x10
 
 	get_second:
-		mov al,cl
+		mov al,dh
 		and al, 0x0f
 		xlat
 		int 0x10
 
+	xchg dh, dl
+
 	get_third:
-		mov al,dl
+		mov al,dh
 		shr al, 4
 		xlat
 		int 0x10
 
 
 	get_fourth:
-		mov al, dl
+		mov al, dh
 		and al, 0x0f
 		xlat
 		int 0x10
 
-
 	popa
 	ret
+
+
+table db '0123456789ABCDEF'
+
+
+
+
+
+
 
 
 
